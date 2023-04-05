dab6d5155df20aeea3ac5cb0e8a4593291f4ce41
Javich23 <jchavezchaidez@appdev.yearup.org>
2023-04-05 17:36:27 -0400

    Completed module

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -30,18 +30,15 @@ public class MortgageCalculator {
     }
     public String toString() {
         DecimalFormat df = new DecimalFormat("####0.00");
-        return "monthlyPayment: " + df.format(getMonthlyInterestRate());
+        return "monthlyPayment: " + df.format(monthlyPayment);
     }
     public static void main(String[] args) {
-        long loanAmount =Long.parseLong(args[0]);
+        long loanAmount = Long.parseLong(args[0]);
         int termInYears = Integer.parseInt(args[1]);
         float annualRate = Float.parseFloat(args[2]);
 
         MortgageCalculator calculator = new MortgageCalculator(loanAmount, termInYears, annualRate);
         calculator.calculateMonthlyPayment();
         System.out.println(calculator.toString());
-
     }
-
-
 }
