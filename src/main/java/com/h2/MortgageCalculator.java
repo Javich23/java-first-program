6faa75c7e0ab5eadc22341b3276ebd23249613bf
Javich23 <jchavezchaidez@appdev.yearup.org>
2023-04-05 19:02:17 -0400

    Completed module

--- src/main/java/com/h2/MortgageCalculator.java
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -28,6 +28,11 @@ public class MortgageCalculator {
         double M = P * (((r * Math.pow(1 + r, n))) / ((Math.pow((1 + r), n)) - 1));
         this.monthlyPayment = M;
     }
+    public String toString() {
+
+        DecimalFormat df = new DecimalFormat("####0.00");
+        return "monthlyPayment: " + df.format(monthlyPayment);
+    }
     public static void main(String[] args) {
         long loanAmount = Long.parseLong(args[0]);
         int termInYears = Integer.parseInt(args[1]);
