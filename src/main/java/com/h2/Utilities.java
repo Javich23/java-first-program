7c3a662a448749771b038dfa4afe5d3961c622ca
Javich23 <jchavezchaidez@appdev.yearup.org>
2023-04-05 22:34:31 -0400

    Completed module

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -7,7 +7,7 @@ public class Utilities {
             out = Long.parseLong(in);
         }
         catch (NumberFormatException e) {
-            throw new IllegalArgumentException(in + "cannot be converted into a 'long' value. Exiting program");
+            throw new IllegalArgumentException(in + " cannot be converted into a 'long' value. Exiting program");
         }
         return out;
     }
@@ -17,7 +17,7 @@ public class Utilities {
             out = Integer.parseInt(in);
         }
         catch (NumberFormatException e) {
-            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program");
+            throw new IllegalArgumentException(in + " cannot be converted into a 'int' value. Exiting program.");
         }
         return out;
     }
